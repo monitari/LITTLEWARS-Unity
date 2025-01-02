14fc6ac50da482391657bd1ee6e68df1f9560451
monitari <qualified7389@gmail.com>
2025-01-03 01:30:04 +0900

    add Source

---
+++ Assets/Scripts/MyType.cs
@@ -0,0 +1,5 @@
+public class MyType
+{
+    public enum Unit { Sword, Range, Guard, Wizard, Bullet };
+    public enum Sound { Victory, Defeat, Buy, Upgrade, Sword, Range, RangeHit, Guard, Magic }; 
+}
